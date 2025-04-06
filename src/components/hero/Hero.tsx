5eb29b8543939111c8c54859c3687b82638dcbbe
tfennelly <tom.fennelly@gmail.com>
2025-04-06 20:52:02 +0100

    main hero tweaks

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -9,11 +9,14 @@ export default function Hero() {
                 <h1 className={GeistSans.className} style={{fontSize: "5rem"}}>Mikey Fennelly</h1>
                 <div className={"small-separator"}>
                     <p>
-                        I'm a passionate technologist with a keen interest in Software Engineering, Artificial Intelligence, video creation and graphics.
-                        While actively pursuing these interests, I'm also enjoying college life at the <span className="bold">University of Limerick</span> where I'm studying <span className="bold">Immersive Software Engineering [ISE]</span>.
+                        A passionate technologist with a keen interest in Software Engineering, Artificial Intelligence, video creation and graphics.
                     </p>
 
                     <p className={"medium-separator"}>
+                        While actively pursuing these interests, I'm also enjoying college life at the <span className="bold">University of Limerick</span>, where I'm studying <span className="bold">Immersive Software Engineering [ISE]</span>.
+                    </p>
+
+                    <p>
                         <span className="bold">When I'm not doing these things, I'm thinking about them!</span>
                     </p>
                 </div>
