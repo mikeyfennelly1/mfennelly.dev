d45b1dad5b7d914a053a947c8dedfc20a56f91ea
tfennelly <tom.fennelly@gmail.com>
2025-12-31 10:59:01 +0000

    cards and repo links

--- src/components/github-repo-component/GithubRepo.tsx
+++ src/components/github-repo-component/GithubRepo.tsx
@@ -17,33 +17,35 @@ export interface RepoInterface {
 const Repo = ({ repoName, description, technologies, topics, repoURL }: RepoInterface) => {
     return (
         <>
-                    <div className="repo-card repo-card-drop-shadow">
-                        <div className={"top-card"}>
-                            <img className={"repo-owner-avatar"} src='/profile-photo.png' alt=""/>
-                            <div className={"repo-title-owner-container"}>
-                                <h6 className={`repo-title ${GeistMono.className}` }>{repoName}</h6>
-                                <h6 className={"repo-owner"}>{mikeyFennelly1}</h6>
-                            </div>
-                            <div className={"repo-project-source-code-container"}>
-                                <Link href={repoURL} target={"_blank"}>
-                                    <button className={"repo-project-source-code-button"}><img src="/github-mark.svg" width="16" style={{display: "inline", marginRight: "3px"}}/> Source Code</button>
-                                </Link>
-                            </div>
+            <Link href={repoURL}>
+                <div className="repo-card repo-card-drop-shadow">
+                    <div className={"top-card"}>
+                        <img className={"repo-owner-avatar"} src='/profile-photo.png' alt=""/>
+                        <div className={"repo-title-owner-container"}>
+                            <h6 className={`repo-title ${GeistMono.className}` }>{repoName}</h6>
+                            <h6 className={"repo-owner"}>{mikeyFennelly1}</h6>
                         </div>
-                        <p className={"repo-description small-separator"}>
-                            {description}
-                        </p>
-                        <div className={"repo-topics small-separator"}>
-                            {topics?.map((topic) => (
-                                <span key={Math.random()} className={"repo-topic"}>{topic}</span>
-                            ))}
-                        </div>
-                        <div className={"technologies-container"}>
-                            {technologies?.map((tech) => (
-                                <span key={Math.random()} className={" flex"}>{tech}</span>
-                            ))}
+                        <div className={"repo-project-source-code-container"}>
+                            <Link href={repoURL} target={"_blank"}>
+                                <button className={"repo-project-source-code-button"}><img src="/github-mark.svg" width="16" style={{display: "inline", marginRight: "3px"}}/> Source Code</button>
+                            </Link>
                         </div>
                     </div>
+                    <p className={"repo-description small-separator"}>
+                        {description}
+                    </p>
+                    <div className={"repo-topics small-separator"}>
+                        {topics?.map((topic) => (
+                            <span key={Math.random()} className={"repo-topic"}>{topic}</span>
+                        ))}
+                    </div>
+                    <div className={"technologies-container"}>
+                        {technologies?.map((tech) => (
+                            <span key={Math.random()} className={" flex"}>{tech}</span>
+                        ))}
+                    </div>
+                </div>
+            </Link>
         </>
     );
 };
