9765b2a121ad9e3dcff45243932000023a7c20b0
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-02 12:19:52 +0000

    refactor: change accent colors to pastel salmon pink and gunmetal gray

--- src/components/experience/testimonial/Testimonial.tsx
+++ src/components/experience/testimonial/Testimonial.tsx
@@ -4,14 +4,19 @@ export default function Testimonial() {
     return (
         <>
             <div className="medium-separator container testimonial-container">
-                <p className={"side-note"} style={{fontWeight: 200}}>
-                    "I had the pleasure of supervising Mikey during his internship at Midland Steel, where he consistently demonstrated strong technical skills and a proactive work ethic.
+                <div id={"border-element"} className={"animated-gradient"}></div>
+                <div className={"testimonial "}>
 
-                    Mikey contributed to key projects, including the design and production data comparison report, the development of an IFC Parsing Algorithm, and the architecture design for our Installation Reports Database. He also handled various IT tasks with efficiency and reliability.
+                    <p className={"side-note"} style={{fontWeight: 200}}>
+                        "I had the pleasure of supervising Mikey during his internship at Midland Steel, where he consistently demonstrated strong technical skills and a proactive work ethic.
 
-                    Mikey’s adaptability, quick learning, and collaborative spirit made him a valuable team member. I highly recommend Mikey for any future role—he will be an asset to any team."
-                </p>
-                <p className={"medium-separator side-note"}> - <span className={"bold"}>Anupam Tamrakar</span> - Manager, Research and Development at Midland Steel</p>
+                        Mikey contributed to key projects, including the design and production data comparison report, the development of an IFC Parsing Algorithm, and the architecture design for our Installation Reports Database. He also handled various IT tasks with efficiency and reliability.
+
+                        Mikey’s adaptability, quick learning, and collaborative spirit made him a valuable team member. I highly recommend Mikey for any future role—he will be an asset to any team."
+                    </p>
+                    <p className={"medium-separator side-note"}> - <span className={"bold"}>Anupam Tamrakar</span> - Manager, Research and Development at Midland Steel</p>
+                </div>
+
             </div>
         </>
     )
